d03e14202a4d1f374b5ca70cd2519196b2a95e0b
VinaySavla <savlavinay77@gmail.com>
2025-10-27 22:10:41 +0530

    conf reg closed commented

--- src/components/sections/BannerSection.jsx
+++ src/components/sections/BannerSection.jsx
@@ -131,11 +131,11 @@ const BannerSection = ({
               </div>
               
               <div className="mt-6 flex flex-wrap gap-3">
-                <Link href="https://docs.google.com/forms/d/e/1FAIpQLScYVhWJ5s0hwffZE57OALRwqVFOuxxhCRxc5xraTBhe_yqF2Q/viewform" target="_blank" rel="noopener noreferrer" className="flex-1">
+                {/* <Link href="https://docs.google.com/forms/d/e/1FAIpQLScYVhWJ5s0hwffZE57OALRwqVFOuxxhCRxc5xraTBhe_yqF2Q/viewform" target="_blank" rel="noopener noreferrer" className="flex-1">
                   <button className="w-full bg-orange-500 hover:bg-orange-600 text-white font-medium py-2 px-6 rounded-full transition-colors">
                     Register Now
                   </button>
-                </Link>
+                </Link> */}
                 <button 
                   onClick={() => setIsModalOpen(true)}
                   className="bg-orange-500 hover:bg-orange-600 text-white flex-1 flex items-center justify-center gap-2 border border-orange-500 font-medium py-2 px-6 rounded-full transition-colors"
